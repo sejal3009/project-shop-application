68677a49adae08d5e284c82d28f0166485767842
sejal <sejal3009@gmail.com>
2022-02-24 18:10:36 +0530

    first commit

---
+++ orders/tasks.py
@@ -0,0 +1,16 @@
+from myshop.celery import app
+from django.core.mail import send_mail
+from .models import Order
+
+@app.task
+def order_created(order_id):
+	order = Order.objects.get(id=order_id)
+	subject = f'Order nr. {order.id}'
+	message = f'Dear {order.first_name},\n\n' \
+	f'You have successfully placed an order.' \
+	f'Your order ID is {order.id}.'
+	mail_sent = send_mail(subject,
+	message,
+	'pathsej@gmail.com',
+	[order.email])
+	return mail_sent
